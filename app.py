e9109a03aabe4c49d4614d701e6ab99915331caa
2h311 <kcodepal@outlook.com>
2023-07-08 21:11:32 +0100

    first commit to remote

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template(
+        "index.html", context={"title": "Your One Stop Digital Creative Agency"}
+    )
