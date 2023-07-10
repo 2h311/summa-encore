37509fbad484144d431670ca5699c4ff5559ad0a
2h311 <kcodepal@outlook.com>
2023-07-10 15:20:18 +0100

    working on the contact form right now

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,3 +1,4 @@
-(function(){
+(function () {
+  // this is to make sure the year shown on the page is updated at all times instead of manually updating it.
   document.querySelector(".js-footer-copy").innerText = `©${new Date().getFullYear()} Summa Encore. Your trusted partner for your digital success from`;
 }());
