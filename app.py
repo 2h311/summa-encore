f08043267d7be9c02237764036fe13eeb28d9b12
2h311 <kcodepal@outlook.com>
2023-07-12 09:40:45 +0100

    working on the blog now

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
+from dotenv import load_dotenv
 from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
+query_todo = """
+	query Todos {
+	  todos{
+	    id
+	    completed
+	    createdAt
+	    description
+	  }
+	}
+"""
+
+endpoint = "https://api-ap-southeast-2.hygraph.com/v2/cljvhhz1y029001ue4x1e7m5q/master"
+token = os.get("HYGRAPH_TOKEN")
+headers = {"authorization": f"Bearer {token}"}
 
 
 @app.route("/")
--- script.py
+++
@@ -1,3 +0,0 @@
-import requests
-
-endpoint = "https://api-ap-southeast-2.hygraph.com/v2/cljvhhz1y029001ue4x1e7m5q/master"
