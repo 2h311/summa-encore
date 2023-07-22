9a10db247e09f6e7b07c0cf167e6a9fbcec43cbe
2h311 <kcodepal@outlook.com>
2023-07-22 17:30:03 +0100

    hitting a database together with the websites

--- summa/__init__.py
+++ summa/__init__.py
@@ -2,10 +2,11 @@ import os
 from dotenv import load_dotenv
 
 from flask import Flask, render_template, request
-
+from flask_sqlalchemy import SQLAlchemy
 
 load_dotenv()
 app = Flask(__name__)
+db = SQLAlchemy(app)
 app.config["HYGRAPH_TOKEN"] = os.getenv("HYGRAPH_TOKEN")
 app.config["HYGRAPH_ENDPOINT"] = os.getenv("HYGRAPH_ENDPOINT")
 app.config["SECRET_KEY"] = os.getenv("WTF_SECRET_KEY")
